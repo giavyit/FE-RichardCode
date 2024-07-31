02f459b5d25101f481b2eca19dfa4de0c26cf192
giavyit <57033051+giavyit@users.noreply.github.com>
2024-07-31 22:49:50 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "/myapp",
+  base: '/webamy-app/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
